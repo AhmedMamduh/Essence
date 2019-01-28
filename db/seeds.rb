e7ac007f9c952cc352cee8219f16a25278053b20
Ahmed Mamdouh <ahmed_mu.mamdouh@hotmail.com>
2019-01-28 18:12:22 +0200

    Add activerecord-reset-pk-sequence gem and tests seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Category.delete_all
+Category.destroy_all
+Product.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('categories')
+ActiveRecord::Base.connection.reset_pk_sequence!('products')
+
 clothing = Category.create!(name: 'Clothing')
 shoes = Category.create!(name: 'Shoes')
 accessories = Category.create!(name: 'accessories')
+
+product1 = Product.create!(title: 'Knot Front Mini Dress', image_url: 'product-1.jpg',
+							price: 55, category_id: 1)
+product2 = Product.create!(title: 'Knot Front Mini Dress', image_url: 'product-2.jpg',
+							price: 88, category_id: 2)
